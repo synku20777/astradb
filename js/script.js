9c4580834c6837429664e05978cb50094e3c675e
synku20777 <nestor.kulik@gmail.com>
2024-02-26 14:54:45 +0100

    Add video showcase and text reveal animation

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+// underline
 document.querySelectorAll(".nav-item").forEach((item) => {
   item.addEventListener("mouseover", (event) => {
     event.target.classList.add("hovered");
@@ -8,6 +9,8 @@ document.querySelectorAll(".nav-item").forEach((item) => {
   });
 });
 
+// cursor
+
 var circle = document.querySelector("#cursor");
 var frames = document.querySelectorAll("#frame");
 window.addEventListener("mousemove", function (dets) {
@@ -17,5 +20,25 @@ window.addEventListener("mousemove", function (dets) {
     duration: 0.4,
     opacity: 1,
     ease: Sine,
+  });
+});
+
+// text reveal on section
+let totalSection = document.querySelectorAll("section");
+totalSection.forEach(function (elem, index) {
+  const text = new SplitType(elem.querySelector(".animate-heading"));
+  let textwords = text.words;
+  gsap.from(textwords, {
+    scrollTrigger: {
+      trigger: elem,
+      start: "top top",
+      end: "bottom top",
+      markers: true,
+    },
+    duration: 1,
+    opacity: 0,
+    yPercent: 100,
+    ease: Power3.out,
+    stagger: 0.05,
   });
 });
