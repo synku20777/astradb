af16029abf1bbd7665ab58c1b99596b7bde9dea6
synku20777 <nestor.kulik@gmail.com>
2024-02-29 17:39:00 +0100

    fixed text animation

--- js/script.js
+++ js/script.js
@@ -27,26 +27,32 @@ window.addEventListener("mousemove", function (dets) {
 const options = {
   root: null,
   rootMargin: "0px",
-  threshold: 0.5,
+  threshold: 0.2,
 };
 
 const observer = new IntersectionObserver((entries, observer) => {
   entries.forEach((entry) => {
-    if (entry.isIntersecting) {
-      const text = new SplitType(
-        entry.target.querySelectorAll(".animate-text")
-      );
-      let textwords = text.words;
-      textwords.forEach(function (word, index) {
+    const text = new SplitType(entry.target.querySelectorAll(".animate-text"));
+    let textwords = text.words;
+    textwords.forEach(function (word, index) {
+      if (entry.isIntersecting) {
         gsap.from(word, {
+          opacity: 0,
           delay: 0.02 * index,
           duration: 1,
-          opacity: 0,
           yPercent: 100,
           ease: Power3.out,
           stagger: 0.4 * index,
         });
-      });
+      } else {
+        gsap.to(word, {
+          opacity: 0,
+          duration: 0.5,
+          ease: Power3.out,
+        });
+      }
+    });
+    if (entry.isIntersecting) {
       observer.unobserve(entry.target);
     }
   });
@@ -58,12 +64,17 @@ totalSection.forEach((elem) => {
 });
 
 // smooth scroll
-gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
+gsap.registerPlugin(ScrollTrigger);
 
-const smoother = ScrollSmoother.create({
-  smooth: 2,
-  speed: 2,
-  effects: true,
-  // normalizeScroll: true,
-  smoothTouch: 0.1,
+gsap.to(".render", {
+  y: "-50%",
+  ease: "none",
+  scrollTrigger: {
+    trigger: ".render",
+    start: "top bottom", // when the top of the trigger hits the bottom of the viewport
+    end: "bottom top", // when the bottom of the trigger hits the top of the viewport
+    scrub: true, // smooth scrubbing, takes 1 second to "catch up" to the scrollbar
+    pin: true, // pins the element for the duration of the scroll
+    pinSpacing: false, // disables automatic adjustment of pin spacing
+  },
 });
